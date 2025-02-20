db4f4b1aa81c257d7637ff21fde0213f60f28a41
kidZmeisteR <medenilla.lawrence@gmail.com>
2025-02-20 21:45:05 +0800

    re update init

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./config/db');
 const authRoutes = require('./routes/auth');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
+
+// Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(5000, () => console.log("🚀 Server running on port 5000"));
